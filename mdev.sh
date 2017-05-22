21ce6e736dcd7df87fc550ba04958166f7226797
kangge <zhuoyikang@gmail.com>
2017-05-22 09:40:51 +0800

    sync

--- mdev.sh
+++ mdev.sh
@@ -2,7 +2,8 @@
 tmux kill-session -t mass
 tmux new-session -s mass -d
 tmux new-window -c ~/source/etcd -n etcd 'bash run.sh; read'
-tmux new-window -c ~/source/mongo/mongodb-linux-x86_64-3.4.2/bin -n mongo './mongod --dbpath=/home/kangge/source/mongo/data ; read'
+tmux new-window -c ~/source/mongo -n mongo 'mongod --dbpath=/data/mongo ; read'
 tmux new-window -c ~/source/redis -n redis 'redis-server; read'
-tmux new-window -c ~/source/mass2/trunk/UnityPrj -n omnisharp '~/source/omnisharp-server/OmniSharp/bin/Debug/OmniSharp.exe; read'
+tmux new-window -c ~/source/mass/trunk/UnityPrj -n omnisharp1 'mono ~/source/omnisharp-server/OmniSharp/bin/Debug/OmniSharp.exe; read'
+tmux new-window -c ~/source/mass/trunk/UnityPrj -n omnisharp2 '~/source/omnisharp-server/OmniSharp/bin/Debug/OmniSharp.exe; read'
 tmux attach-session -t mass
