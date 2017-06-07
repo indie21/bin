0ba4bfd30fe646f172b03addd30bdb67647ebff5
kangge <zhuoyikang@gmail.com>
2017-06-07 17:25:29 +0800

    x

--- mdev.sh
+++ mdev.sh
@@ -6,4 +6,5 @@ tmux new-window -c ~/source/mongo -n mongo 'mongod --dbpath=/data/mongo ; read'
 tmux new-window -c ~/source/redis -n redis 'redis-server; read'
 tmux new-window -c ~/source/mass/trunk/UnityPrj -n omnisharp1 'mono ~/source/omnisharp-server/OmniSharp/bin/Debug/OmniSharp.exe; read'
 tmux new-window -c ~/source/mass/trunk/UnityPrj -n omnisharp2 '~/source/omnisharp-server/OmniSharp/bin/Debug/OmniSharp.exe; read'
+tmux new-window -c ~/source/ -n ngrok './ngrok tcp 3101; read'
 tmux attach-session -t mass
