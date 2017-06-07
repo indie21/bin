487ec8d9190f44817220dc3df79b10a20cd743b9
kangge <zhuoyikang@gmail.com>
2017-06-07 17:31:46 +0800

    sync

--- mdev.sh
+++ mdev.sh
@@ -3,7 +3,7 @@ tmux kill-session -t mass
 tmux new-session -s mass -d
 tmux new-window -c ~/source/etcd -n etcd 'bash run.sh; read'
 tmux new-window -c ~/source/mongo -n mongo 'mongod --dbpath=/data/mongo ; read'
-tmux new-window -c ~/source/redis -n redis 'redis-server; read'
+tmux new-window -c ~/source/redis -n redis 'redis-server ~/source/redis/6379.conf --protected-mode no; read'
 tmux new-window -c ~/source/mass/trunk/UnityPrj -n omnisharp1 'mono ~/source/omnisharp-server/OmniSharp/bin/Debug/OmniSharp.exe; read'
 tmux new-window -c ~/source/mass/trunk/UnityPrj -n omnisharp2 '~/source/omnisharp-server/OmniSharp/bin/Debug/OmniSharp.exe; read'
 tmux new-window -c ~/source/ -n ngrok './ngrok tcp 3101; read'
