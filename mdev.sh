d7a7ac75f6acb41ff25079eeee7ec473a244048d
kangge <zhuoyikang@gmail.com>
2019-04-03 09:37:52 +0800

    xx

--- mdev.sh
+++ mdev.sh
@@ -5,7 +5,7 @@ tmux new-window -c ~/source/etcd -n etcd 'bash run.sh; read'
 tmux new-window -c ~/source/mongo -n mongo 'mongod --dbpath=/data/mongo > log.txt ; read'
 tmux new-window -c ~/source/redis -n redis 'redis-server ~/source/redis/6379.conf --protected-mode no; read'
 tmux new-window -c ~/source/ngrok/bin -n ngrok './ngrok -config=ngrok.cfg -proto=tcp 3101 read'
-tmux new-window -c ~/source/catlikecoding -n omnisharp1 'mono ~/source/omnisharp-server/OmniSharp/bin/Debug/OmniSharp.exe; read'
+#tmux new-window -c ~/source/d1 -n omnisharp1 'mono ~/source/omnisharp-server/OmniSharp/bin/Debug/OmniSharp.exe; read'
 #tmux new-window -c ~/source/gts/tr -n omnisharp2 '~/source/omnisharp-server/OmniSharp/bin/Debug/OmniSharp.exe; read'
 
 tmux attach-session -t mass
